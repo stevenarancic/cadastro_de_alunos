1b59094498aaeda4d58e2643de53cf800f969c04
Steve Narancic <58119793+stevenarancic@users.noreply.github.com>
2021-08-23 23:23:29 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/cadastro_de_alunos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cadastro_de_alunos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:cadastro_de_alunos/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
